bd24855ba88eb8158997e9d7150156d545f37725
markmiw <markmiw6@gmail.com>
2022-06-07 12:27:27 -1000

    Minimum req done

--- src/blueDancer.js
+++ src/blueDancer.js
@@ -1,7 +1,9 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var blueDancer = function(top, left, timeBetweenSteps) {
 
   makeDancer.call(this, top, left, timeBetweenSteps);
-
+  //this.color = '10px solid blue';
+  this.setColor();
+  //this.setColor('10px solid blue');
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
@@ -13,10 +15,10 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   //return blinkyDancer;
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+blueDancer.prototype = Object.create(makeDancer.prototype);
+blueDancer.prototype.constructor = blueDancer;
 
-makeBlinkyDancer.prototype.step = function() {
+blueDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
   // toggle() is a jQuery method to show/hide the <span> tag.
@@ -25,24 +27,24 @@ makeBlinkyDancer.prototype.step = function() {
   this.$node.toggle();
 };
 
-makeBlinkyDancer.prototype.lineup = function(top, left) {
-  // call the old version of step at the beginning of any call to this new version of step
-  makeDancer.prototype.setPosition.call(this, top, left);
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  //this.$node.toggle();
-};
-
-makeBlinkyDancer.prototype.setColor = function(color) {
+blueDancer.prototype.setColor = function(color) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   if (!color) {
-    color = 'red';
+    color = 'blue';
   }
   let str = '10px solid ' + color;
   var styleSettings = {
     border: str
   };
   this.$node.css(styleSettings);
+};
+
+blueDancer.prototype.lineup = function(top, left) {
+  // call the old version of step at the beginning of any call to this new version of step
+  makeDancer.prototype.setPosition.call(this, top, left);
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  //this.$node.toggle();
 };
--- src/init.js
+++ src/init.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   window.dancers = [];
-
+  window.objectDistances = [];
+  //Original Blinky Button
   $('.addDancerButton').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
@@ -15,19 +16,145 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    var dancerMakerFunctionName = $(this).data
+    ('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
-
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    window.dancers.push(dancer);
+    window.objectDistances.push([dancer.top, dancer.left]);
+    for (let i = 0; i < window.objectDistances.length; i++) {
+      let [Obj1top, Obj1left] = window.objectDistances[i];
+      for (let j = i + 1; j < window.objectDistances.length; j++) {
+        debugger;
+        let [Obj2top, Obj2left] = window.objectDistances[j];
+        let distance = Math.pow(Math.pow((Obj2left - Obj1left), 2) + Math.pow((Obj2top - Obj1top), 2), .5);
+        if (distance < 100) {
+          window.dancers[i].setColor('pink');
+          window.dancers[j].setColor('pink');
+          $('body').append(window.dancers[i].$node);
+          $('body').append(window.dancers[j].$node);
+        }
+      }
+    }
     $('body').append(dancer.$node);
   });
+
+  //Blue Dancer Button
+  $('.addBlueDancerButton').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data
+    ('dancer-maker-function-name');
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+    var dancer = new dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    window.dancers.push(dancer);
+    window.objectDistances.push([dancer.top, dancer.left]);
+    for (let i = 0; i < window.objectDistances.length; i++) {
+      let [Obj1top, Obj1left] = window.objectDistances[i];
+      for (let j = i + 1; j < window.objectDistances.length; j++) {
+        debugger;
+        let [Obj2top, Obj2left] = window.objectDistances[j];
+        let distance = Math.pow(Math.pow((Obj2left - Obj1left), 2) + Math.pow((Obj2top - Obj1top), 2), .5);
+        if (distance < 100) {
+          window.dancers[i].setColor('pink');
+          window.dancers[j].setColor('pink');
+          $('body').append(window.dancers[i].$node);
+          $('body').append(window.dancers[j].$node);
+        }
+      }
+    }
+    $('body').append(dancer.$node);
+  });
+
+
+  //Yellow Dancer Button
+  $('.addYellowDancerButton').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data
+    ('dancer-maker-function-name');
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+    var dancer = new dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    window.dancers.push(dancer);
+    window.objectDistances.push([dancer.top, dancer.left]);
+    for (let i = 0; i < window.objectDistances.length; i++) {
+      let [Obj1top, Obj1left] = window.objectDistances[i];
+      for (let j = i + 1; j < window.objectDistances.length; j++) {
+        debugger;
+        let [Obj2top, Obj2left] = window.objectDistances[j];
+        let distance = Math.pow(Math.pow((Obj2left - Obj1left), 2) + Math.pow((Obj2top - Obj1top), 2), .5);
+        if (distance < 100) {
+          window.dancers[i].setColor('pink');
+          window.dancers[j].setColor('pink');
+          $('body').append(window.dancers[i].$node);
+          $('body').append(window.dancers[j].$node);
+        }
+      }
+    }
+    $('body').append(dancer.$node);
+  });
+
+  //Line Up Button
+  $('.lineUpButton').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data
+    ('operate-dancer-function-name');
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+    for (var i = 0; i < window.dancers.length; i++) {
+      let ypos = (i * 20);
+      let yposstr = ypos + 'px';
+      let xpos = 300;
+      let xposstr = xpos + 'px';
+      window.objectDistances[i] = [ypos, xpos];
+      var dancer = window.dancers[i];
+      dancer.lineup(yposstr, xposstr);
+    }
+    $('.dancer').remove();
+    // for (var i = 0; i < window.dancers.length; i++) {
+    //   dancer = window.dancers[i];
+    //   window.
+    //   $('body').append(dancer.$node);
+    // }
+
+    for (let i = 0; i < window.objectDistances.length; i++) {
+      let [Obj1top, Obj1left] = window.objectDistances[i];
+      for (let j = i + 1; j < window.objectDistances.length; j++) {
+        debugger;
+        let [Obj2top, Obj2left] = window.objectDistances[j];
+        let distance = Math.pow(Math.pow((Obj2left - Obj1left), 2) + Math.pow((Obj2top - Obj1top), 2), .5);
+        if (distance < 100) {
+          window.dancers[i].setColor('pink');
+          window.dancers[j].setColor('pink');
+          $('body').append(window.dancers[i].$node);
+          $('body').append(window.dancers[j].$node);
+        }
+      }
+    }
+
+  });
+
 });
 
